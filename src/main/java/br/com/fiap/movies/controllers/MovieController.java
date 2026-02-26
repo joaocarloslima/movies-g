0cb0f7c93c32280dcc7b297b78ff6aa02cfd4cd8
joaocarloslima <joaocarloslima@me.com>
2026-02-26 15:51:29 -0300

    add Movie model and MovieController with health check and movie listing endpoints

--- src/main/java/br/com/fiap/movies/controllers/MovieController.java
+++ src/main/java/br/com/fiap/movies/controllers/MovieController.java
@@ -1,19 +1,46 @@
 package br.com.fiap.movies.controllers;
 
+import br.com.fiap.movies.models.Movie;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @RestController
+@Slf4j
 public class MovieController {
 
+    record HealthStatus(String status, String message){}
+
     @GetMapping("/")
-    public String healthCheck() {
-        return """
-                { 
-                    "status": "ok",
-                    "message": "API is running"
-                }
-                """;
+    public HealthStatus healthCheck() {
+        return new HealthStatus("OK", "API is running");
+    }
+
+    @GetMapping("/movies")
+    public List<Movie> listAll(){
+        return List.of( new Movie(
+                "Titanic",
+                5,
+                "Todos morrem no final",
+                LocalDate.of(1997, 12, 01),
+                "2h30",
+                "Romance"
+        ));
+    }
+
+    @PostMapping("/movies")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Movie createMovie(@RequestBody Movie movie){ //binding
+        log.info("Cadastrando filme..." + movie);
+        return movie;
     }
 
 }
---
+++ src/main/java/br/com/fiap/movies/models/Movie.java
@@ -0,0 +1,17 @@
+package br.com.fiap.movies.models;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+public class Movie {
+    private String title;
+    private int rating;
+    private String synopsis;
+    private LocalDate releaseDate;
+    private String duration;
+    private String genre;
+
+}
